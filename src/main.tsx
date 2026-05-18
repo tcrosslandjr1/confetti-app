6d9f03bb119b76db88f8b1a1377434f44299196f
tcrosslandjr1 <tcrosslandjr1@gmail.com>
2026-05-18 19:46:55 -0400

    Initialize main.tsx with React Router setup

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "@tanstack/react-router";
+import { getRouter } from "./router";
+import "./styles.css";
+
+const router = getRouter();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
+  );

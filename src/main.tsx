dfc3ad0795b8570e788459fa6a5ab5f10e503f18
tcrosslandjr1 <tcrosslandjr1@gmail.com>
2026-05-18 20:08:36 -0400

    Delete src/main.tsx

--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "@tanstack/react-router";
-import { getRouter } from "./router";
-import "./styles.css";
-
-const router = getRouter();
-
-ReactDOM.createRoot(document.getElementById("root")!).render(<RouterProvider router={router} />);

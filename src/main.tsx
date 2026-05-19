538496f7fef6d3cdb763712722c736e6584e5e69
tcrosslandjr1 <tcrosslandjr1@gmail.com>
2026-05-18 21:42:52 -0400

    Add SPA client entry point for Vite build

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "@tanstack/react-router";
+import { getRouter } from "./router";
+import "./styles.css";
+
+const router = getRouter();
+
+const rootElement = document.getElementById("root")!;
+
+ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
